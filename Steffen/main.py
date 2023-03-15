8843b178ad1893326fee8167f3d04ceba8695a6a
Fernando3161 <fernandoandres.penaherreravaca@offis.de>
2023-03-15 15:43:01 +0100

    Format

--- Steffen/main.py
+++ Steffen/main.py
@@ -15,8 +15,8 @@ def create_parents(mu, N, sigma, scaling_factor):
     parents: [Organism] = []
     for k in range(mu):
         z_k = np.random.rand(n)
-        x_k =  z_k * scaling_factor
-        parent = Organism(fit=fitn(x_k), x = x_k, sigma=sigma, z_k=z_k) # Hier
+        x_k = z_k * scaling_factor
+        parent = Organism(fit=fitn(x_k), x=x_k, sigma=sigma, z_k=z_k)  # Hier
         parents.append(parent)
     return parents
 
@@ -28,6 +28,7 @@ def create_children(best_parent, sigma, fitn, generation, n, C, muta, lambd):
         child = muta(best_parent, sigma, fitn, generation, n, C)
         children.append(child)
     return children
+
 
 def create_children_cross(lambd, parents, rho, sigma, fitn, generation, n):
     children: [Organism] = []
@@ -48,14 +49,16 @@ def crossover(Parents, rho):
     sum_x = 0
     sum_sigma = 0
     for i in range(rho):
-      newparent:  Organism = random.choice(Parents)
-      newparents.append(newparent)
-      sum_x += newparents[i].x
-      sum_sigma += newparents[i].sigma
+        newparent:  Organism = random.choice(Parents)
+        newparents.append(newparent)
+        sum_x += newparents[i].x
+        sum_sigma += newparents[i].sigma
     child_x = sum_x / rho
     child_sigma = sum_sigma / rho
-    child = Organism(fit=fitn(child_x), x=child_x, born=generation, sigma=child_sigma)
+    child = Organism(fit=fitn(child_x), x=child_x,
+                     born=generation, sigma=child_sigma)
     return child
+
 
 def cma_plus(parents):
     mu = np.sqrt(1 / (n + 1))
@@ -71,58 +74,62 @@ if __name__ == '__main__':
     n = 2  # Genomes / dimensions
     mu = 1  # μ: Parents
     lambd = 10  # λ: Offsprings
-    max_generation = 2000 # Set maximum generation
+    max_generation = 2000  # Set maximum generation
 
-    rho = 2 # ρ: Parent population
-    scaling_factor = 5 # scaling factor for the initial parents
+    rho = 2  # ρ: Parent population
+    scaling_factor = 5  # scaling factor for the initial parents
     print(f'With: μ={mu}, λ={lambd} and {n} dimension(s).')
 
     "Rechenberg Rule"
-    #rechenberg = 1 # Switch on and off for the rule
+    # rechenberg = 1 # Switch on and off for the rule
     d = np.sqrt(n+1)
 
     "Initial parameters for evolution path"
-    #s_sigma = np.zeros(n) # Initial evolution path
+    # s_sigma = np.zeros(n) # Initial evolution path
 
     "Initial parameters for CMA"
 
-
-    plot_generation = 1 # Do we want plots?
+    plot_generation = 1  # Do we want plots?
 
     "Iteration over fitness functions"
-    for fitn in [ f.rosenbruck]:# [f.sphere, f.rastrigen, f.rosenbruck]: #[f.sphere, f.rastrigen, f.rosenbruck, f.doublesum]:
+    # [f.sphere, f.rastrigen, f.rosenbruck]: #[f.sphere, f.rastrigen, f.rosenbruck, f.doublesum]:
+    for fitn in [f.rosenbruck]:
         print(f'\n')
-        print(f'-------------------- Results for {fitn.__name__} fitness function. --------------------')
+        print(
+            f'-------------------- Results for {fitn.__name__} fitness function. --------------------')
 
         "Iteration over mutation functions"
-        for muta in [m.gaus_muta, m.self_adap, m.dr_self_adap, m.evol_path, m.cma]:  # [m.gaus_muta, m.self_adap, m.dr_self_adap, m.evol_path, m.cma]
+        # [m.gaus_muta, m.self_adap, m.dr_self_adap, m.evol_path, m.cma]
+        for muta in [m.gaus_muta, m.self_adap, m.dr_self_adap, m.evol_path, m.cma]:
             #[ m.evol_path ]:#
             "Initial value assignments"
-            generation = 0 # Set generation counter back to zero
+            generation = 0  # Set generation counter back to zero
             iteration = 0
-            sigma = 1 / n # Mutation rates (also called stepsize)
-            solution_list = [] # Set list of best solutions back to empty
-            sigma_list = [] # Set list of best solutions sigmas back to empty
-            s_sigma = np.zeros(n) 
+            sigma = 1 / n  # Mutation rates (also called stepsize)
+            solution_list = []  # Set list of best solutions back to empty
+            sigma_list = []  # Set list of best solutions sigmas back to empty
+            s_sigma = np.zeros(n)
             rechenberg = 1
-            A = [] # archive A of the α best solutions
-            C = np.identity(n)  # correlation matrix which specifies correlations between dimensions
-            kappa = 20 #
+            A = []  # archive A of the α best solutions
+            # correlation matrix which specifies correlations between dimensions
+            C = np.identity(n)
+            kappa = 20
             alpha = 40  # α best solutions
             "Creation of first parent generation"
             parents = create_parents(mu, n, sigma, scaling_factor)
             best_parent = sorted(parents, key=lambda x: x.fit)[0]
             old_parent = best_parent
 
-            dt1 = datetime.datetime.now() # Get first timestemp for duration calculation
+            dt1 = datetime.datetime.now()  # Get first timestemp for duration calculation
             while generation < max_generation:
                 generation += 1
 
                 "CMA: adapt C Matrix every kappa generations"
-                if generation%kappa == 0 and len(A) >= alpha:
+                if generation % kappa == 0 and len(A) >= alpha:
                     C = np.cov(np.transpose(A))
 
-                children = create_children(old_parent, sigma, fitn, generation, n, C, muta, lambd=lambd)
+                children = create_children(
+                    old_parent, sigma, fitn, generation, n, C, muta, lambd=lambd)
 
                 "Choose selection for each mutation"
                 if muta == m.gaus_muta:
@@ -131,22 +138,23 @@ if __name__ == '__main__':
                     new_parents = s.comma(mu, old_parent, children)
                 elif muta == m.dr_self_adap:
                     new_parents = s.comma(mu, old_parent, children)
-                elif muta == m.evol_path: # I think that the selection here is not right
+                elif muta == m.evol_path:  # I think that the selection here is not right
                     new_parents = s.comma(mu, old_parent, children)
                 elif muta == m.cma:
                     new_parents = s.plus(mu, old_parent, children)
 
-                best_parent = sorted(new_parents, key=lambda x: x.fit, reverse=False)[0]
+                best_parent = sorted(
+                    new_parents, key=lambda x: x.fit, reverse=False)[0]
 
                 "Rechenberg success counter function and sigma adaption for gaus and CMA"
                 if muta == m.gaus_muta:
                     if rechenberg == 1:
-                        sigma = m.rechenberg(best_parent,old_parent,sigma,d)
+                        sigma = m.rechenberg(best_parent, old_parent, sigma, d)
                 elif muta == m.cma:
-                    sigma = m.rechenberg(best_parent,old_parent,sigma,d)
+                    sigma = m.rechenberg(best_parent, old_parent, sigma, d)
 
                 "Comparision between best old parents and new parents"
-                if best_parent.fit < old_parent.fit and  muta != m.evol_path:
+                if best_parent.fit < old_parent.fit and muta != m.evol_path:
                     old_parent = best_parent
                     "CMA: A adaption"
                     if muta == m.cma:
@@ -171,9 +179,11 @@ if __name__ == '__main__':
                     sigma = old_parent.sigma
                     "Value assignments after selection"
                     s_sigma = (1 - c_sigma) * s_sigma + c_sigma * z  # eq. 9
-                    
+
                     "Similar equation formulations - last one works best"
-                    sigma = sigma * np.exp((c_sigma / d) * (np.linalg.norm(s_sigma) / np.sqrt(n) - 1))  # eq. 10
+                    sigma = sigma * \
+                        np.exp(
+                            (c_sigma / d) * (np.linalg.norm(s_sigma) / np.sqrt(n) - 1))  # eq. 10
                     # sigma = old_parent.sigma * np.exp((c_sigma / (2 * d)) * (np.linalg.norm(s_sigma ** 2) / n - 1))  # eq. 10
                     # sigma = old_parent.sigma * np.exp(1 / 2 / d / n * ((np.linalg.norm(s_sigma)) ** 2 - n))  # eq. 10
                     old_parent = best_parent
@@ -184,25 +194,30 @@ if __name__ == '__main__':
                     sigma_list.append(old_parent.sigma)
 
                 "Exit if no improvement of results"
-                #if len(solution_list) > 100: # 21
+                # if len(solution_list) > 100: # 21
                 #    mean_before = np.mean(solution_list[-100:-2]) # [-21:-2])
                 #    mean_after = np.mean(solution_list[-99:-1]) # [-20:-1])
                 #    if mean_after == mean_before:
                 #        max_generation = generation
 
-            dt2 = datetime.datetime.now() # Get last timestemp for duration calculation
-            dt = dt2 - dt1 # Get timestep delta
+            dt2 = datetime.datetime.now()  # Get last timestemp for duration calculation
+            dt = dt2 - dt1  # Get timestep delta
             "Exit print after finishing"
             if muta == m.gaus_muta:
-                print(f'({mu}+{lambd}) Gaussian mutation: {old_parent.fit} from generation: {old_parent.born} Duration: {dt.seconds} seconds')
+                print(
+                    f'({mu}+{lambd}) Gaussian mutation: {old_parent.fit} from generation: {old_parent.born} Duration: {dt.seconds} seconds')
             elif muta == m.self_adap:
-                print(f'({mu},{lambd}) Self-Adaptation (SA): {old_parent.fit} from generation: {old_parent.born} Duration: {dt.seconds} seconds')
+                print(
+                    f'({mu},{lambd}) Self-Adaptation (SA): {old_parent.fit} from generation: {old_parent.born} Duration: {dt.seconds} seconds')
             elif muta == m.dr_self_adap:
-                print(f'({mu},{lambd}) De-randomized SA: {old_parent.fit} from generation: {old_parent.born} Duration: {dt.seconds} seconds')
+                print(
+                    f'({mu},{lambd}) De-randomized SA: {old_parent.fit} from generation: {old_parent.born} Duration: {dt.seconds} seconds')
             elif muta == m.evol_path:
-                print(f'({mu},{lambd}) Evolution path: {old_parent.fit} from generation: {old_parent.born} Duration: {dt.seconds} seconds')
+                print(
+                    f'({mu},{lambd}) Evolution path: {old_parent.fit} from generation: {old_parent.born} Duration: {dt.seconds} seconds')
             elif muta == m.cma:
-                print(f'({mu}+{lambd}) Covariance matrix adaption: {old_parent.fit} from generation: {old_parent.born} Duration: {dt.seconds} seconds')
+                print(
+                    f'({mu}+{lambd}) Covariance matrix adaption: {old_parent.fit} from generation: {old_parent.born} Duration: {dt.seconds} seconds')
 
             "Plot generation"
             if plot_generation == 1:
@@ -223,6 +238,7 @@ if __name__ == '__main__':
 
                 "Plot saving"
                 if best_parent.fit < 0.000000000000001:
-                    plt.savefig(f'Plots\{fitn.__name__}_{muta.__name__}_convert.png')
+                    plt.savefig(
+                        f'Plots\{fitn.__name__}_{muta.__name__}_convert.png')
                 else:
                     plt.savefig(f'Plots\{fitn.__name__}_{muta.__name__}.png')
--- Steffen/mutation.py
+++ Steffen/mutation.py
@@ -2,7 +2,7 @@ import numpy as np
 from Organism import Organism
 
 
-def gaus_muta(child, sigma, fitn, generation, n,C):
+def gaus_muta(child, sigma, fitn, generation, n, C):
     """
     Uses the self-adaptation for the mutation of genomes.
 
@@ -22,12 +22,15 @@ def gaus_muta(child, sigma, fitn, generation, n,C):
     child = Organism(fit=fitn(x_k), x=x_k, born=generation, sigma=sigma)
     return child
 
-def rechenberg(best_parent,old_parent,sigma,d):
+
+def rechenberg(best_parent, old_parent, sigma, d):
     faktor = 1 if best_parent.fit < old_parent.fit else 0
     sigma = sigma * np.exp(1 / d * (faktor - 1 / 5))
     return sigma
 
 # ToDo: N optional parameter
+
+
 def self_adap(child, sigma, fitn, generation, n, C):
     """
     Uses the self-adaptation for the mutation of genomes.
@@ -49,7 +52,8 @@ def self_adap(child, sigma, fitn, generation, n, C):
     z_k = np.random.randn(n)  # eq. 6
     sigma_k = sigma * np.exp(e_k)  # eq. 7
     x_k = child.x + sigma_k * z_k  # eq. 8
-    child = Organism(fit=fitn(x_k), x=x_k, born=generation, sigma=sigma_k, z_k=z_k)
+    child = Organism(fit=fitn(x_k), x=x_k, born=generation,
+                     sigma=sigma_k, z_k=z_k)
     return child
 
 
@@ -73,9 +77,10 @@ def multi_reco(child, sigma, fitn, generation, n, C):
     tau_i = 1 / n ** (1/4)
     e_k = tau_i * np.random.randn()  # eq. 5
     z_k = np.random.randn(n)  # eq. 6
-    sigma_k = sigma * np.exp(e_k) #* np.exp(e_k) # eq. 7 # parent.sigma
+    sigma_k = sigma * np.exp(e_k)  # * np.exp(e_k) # eq. 7 # parent.sigma
     x_k = child.x + sigma_k * z_k  # eq. 8
-    child = Organism(fit=fitn(x_k), x=x_k, born=generation, sigma=sigma_k, z_k=z_k)
+    child = Organism(fit=fitn(x_k), x=x_k, born=generation,
+                     sigma=sigma_k, z_k=z_k)
     return child
 
 
@@ -95,13 +100,15 @@ def dr_self_adap(child, sigma, fitn, generation, n, C):
     -------
     child (class): Individual Organsim.
     """
-    tau = 1 / 3 # 0,3333
-    d = np.sqrt(n) # 1,3
-    d_i = n # 10
+    tau = 1 / 3  # 0,3333
+    d = np.sqrt(n)  # 1,3
+    d_i = n  # 10
     e_k = tau * np.random.randn()  # eq. 5
     z_k = np.random.randn(n)  # eq. 6
     x_k = child.x + np.exp(e_k) * sigma * z_k  # eq. 7
-    sigma_k = child.sigma * np.exp((1/d_i) * (np.linalg.norm(z_k) / np.sqrt(n-1) - 1)) * np.exp(e_k / d) # eq. 8 Lehrbuch
+    sigma_k = child.sigma * \
+        np.exp((1/d_i) * (np.linalg.norm(z_k) / np.sqrt(n-1) - 1)) * \
+        np.exp(e_k / d)  # eq. 8 Lehrbuch
     child = Organism(fit=fitn(x_k), x=x_k, born=generation, sigma=sigma_k)
     return child
 
@@ -109,7 +116,8 @@ def dr_self_adap(child, sigma, fitn, generation, n, C):
 def evol_path(child, sigma, fitn, generation, n, C):
     z_k = np.random.randn(n)
     x_k = child.x + sigma * z_k  # eq. 6.1
-    child = Organism(fit=fitn(x_k), x=x_k, born=generation, sigma=sigma, z_k=z_k)
+    child = Organism(fit=fitn(x_k), x=x_k,
+                     born=generation, sigma=sigma, z_k=z_k)
     return child
 
 
